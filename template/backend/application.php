3b3c34615f11b6ceaf02f0abc9a773233ff28410
lemoni-ir <info@lemoni.ir>
2025-03-06 16:15:20 +0330

    change application code

--- template/backend/application.php
+++ template/backend/application.php
@@ -10,13 +10,6 @@ class application
     public static function up()
     {
     }
-/*************  ✨ Codeium Command ⭐  *************/
-    /**
-     * Runs the application.
-     *
-     * @throws Exception
-     */
-/******  0e8658e9-ae06-473d-8347-1225aeb3e91a  *******/
     public static function down()
     {
     }
